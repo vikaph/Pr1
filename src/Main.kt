d85f94a90a1a16eb9145c5e34446f0262a27b660
357-22 <vika.p.2006@bk.ru>
2024-09-21 11:49:08 +0300

    гТОВОЕ РЕШЕНИЕ

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Введите строчку: ")
+    val Strochka = readLine()  ?:""
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    var currentChar = Strochka[0]
+    var count = 1
+    val result = StringBuilder()
+
+    for (i in 1 until Strochka.length) {
+        if (Strochka[i] == currentChar) {
+            count++
+        } else { result.append(currentChar)
+            if (count>1) {
+                result.append(count)
+            }
+            currentChar = Strochka[i]
+            count=1
+        }
     }
+    result.append(currentChar)
+    if (count>1) {
+        result.append(count)
+    }
+    println("Количество подряд идущих символов: ${result.toString()}")
 }
